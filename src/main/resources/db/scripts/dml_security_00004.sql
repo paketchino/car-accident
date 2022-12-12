bb258e15f37d1586f39018a852c42f01d2c0e422
paketchino <chansgorn@gmail.com>
2022-12-12 20:15:57 +0300

    0. Initial

--- src/main/resources/db/scripts/dml_security_00004.sql
+++ src/main/resources/db/scripts/dml_security_00004.sql
@@ -1,11 +1,11 @@
 insert into users (username, password, enabled, authority_id)
-values ('username', '$2a$10$wY1twJhMQjGVxv4y5dBC5ucCBlzkzT4FIGa4FNB/pS9GaXC2wm9/W', false,
+values ('username', '$2b$10$wY1twJhMQjGVxv4y5dBC5ucCBlzkzT4FIGa4FNB/pS9GaXC2wm9/W', false,
         (select id from authorities where authority = 'ROLE_USER'));
 
 insert into users (username, password, enabled, authority_id)
-values ('supervisor', '$2a$10$wY1twJhMQjGVxv4y5dBC5ucCBlzkzT4FIGa4FNB/pS9GaXC2wm9/W', false,
+values ('supervisor', '$2b$10$wY1twJhMQjGVxv4y5dBC5ucCBlzkzT4FIGa4FNB/pS9GaXC2wm9/W', false,
         (select id from authorities where authority = 'ROLE_SUPERVISOR'));
 
 insert into users (username, password, enabled, authority_id)
-values ('root', '$2a$10$wY1twJhMQjGVxv4y5dBC5ucCBlzkzT4FIGa4FNB/pS9GaXC2wm9/W', false,
+values ('root', '$2b$10$wY1twJhMQjGVxv4y5dBC5ucCBlzkzT4FIGa4FNB/pS9GaXC2wm9/W', false,
         (select id from authorities where authority = 'ROLE_ADMIN'));
